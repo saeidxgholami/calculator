5ae1300b933d246374b4ab695f562d4593df4e06
Saeid Gholami <saeidxgholami@gmail.com>
2020-12-16 20:36:06 +0330

    set text of the clicked button to equeation_box

--- pyqt_calc/pyqt_ui.py
+++ pyqt_calc/pyqt_ui.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 
 class Calculator:
@@ -11,8 +11,20 @@ class Calculator:
 
     def calculate(self):
         """ calcualte the result of the equation """
-        eq_result = eval(self.equation_box.text())
-        self.equation_box.setText(str(eq_result))
+
+        # check if there is equation in the equation_box
+        if len(self.equation_box.text()) > 0:
+            eq_result = eval(self.equation_box.text())
+            self.equation_box.setText(str(eq_result))
+
+    def clear_one(self):
+        """ Remove one charcter of equation_box from the right """
+        if len(self.equation_box.text()) > 0:
+            self.equation_box.setText(self.equation_box.text()[:-1])
+
+    def clear_all(self):
+        """ Clear all text inside equation_box """
+        self.equation_box.clear()
 
 
 class Ui(QtWidgets.QWidget):
@@ -20,11 +32,24 @@ class Ui(QtWidgets.QWidget):
         super(Ui, self).__init__()
         uic.loadUi("calculator.ui", self)
         self.show()
+        # set focus to equation_box
+        self.equation_box.setFocus()
         self.calculator = Calculator(self.equation_box)
+
+        # call buttonClicked event insert each button text to equation button
+        for child in self.findChildren(QtWidgets.QPushButton):
+            child.clicked.connect(self.buttonClicked)
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Return:
             self.calculator.calculate()
+
+    def buttonClicked(self):
+        sender = self.sender()
+
+        # make the = symbol not inserted to the equation_box after calualte the equation.
+        if sender.text() != "=":
+            self.equation_box.setText(self.equation_box.text() + sender.text())
 
 
 def main():
@@ -34,6 +59,8 @@ def main():
     calculater = Calculator(window.equation_box)
 
     window.equal_button.clicked.connect(calculater.calculate)
+    window.clear_one.clicked.connect(calculater.clear_one)
+    window.clear_all.clicked.connect(calculater.clear_all)
 
     app.exec_()
 
